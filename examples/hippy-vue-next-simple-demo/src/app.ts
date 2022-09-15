1fe661d5cbfa3c676744e4fa90fe4c897fd90857
birdguo <birdguo@tencent.com>
2022-09-15 15:32:47 +0800

    feat(hippy-vue-next-simple): update demo

--- examples/hippy-vue-next-simple-demo/src/app.ts
+++ examples/hippy-vue-next-simple-demo/src/app.ts
@@ -1,43 +1,43 @@
-import { createApp, type NativeNode, NodeOperateType, renderToNative, HippyDocument, HippyElement } from '@hippy/vue-next-simple';
+import { createApp, HippyDocument } from '@hippy/vue-next-simple';
 
 // 创建APP
 createApp({
   appName: 'Demo',
 }).then(({ rootViewId, superProps }) => {
   console.log('received: ', rootViewId, superProps);
-  const nativeNodeOne = {
-    id: 1,
-    pId: rootViewId,
-    index: 0,
-    name: 'Text',
-    props: {
-      text: 'Hello1',
-    },
-  };
-  const nativeNodeTwo = {
-    id: 2,
-    pId: rootViewId,
-    index: 1,
-    name: 'Text',
-    props: {
-      text: 'Hello2',
-    },
-  };
-  const nativeNodeThree = {
-    id: 3,
-    pId: rootViewId,
-    index: 2,
-    name: 'Text',
-    props: {
-      text: 'Hello3',
-    },
-  };
-  // 初始化完成，插入节点
-  const insertNodes: Array<NativeNode> = [
-    nativeNodeOne,
-    nativeNodeTwo,
-    nativeNodeThree,
-  ];
+  // const nativeNodeOne = {
+  //   id: 1,
+  //   pId: rootViewId,
+  //   index: 0,
+  //   name: 'Text',
+  //   props: {
+  //     text: 'Hello1',
+  //   },
+  // };
+  // const nativeNodeTwo = {
+  //   id: 2,
+  //   pId: rootViewId,
+  //   index: 1,
+  //   name: 'Text',
+  //   props: {
+  //     text: 'Hello2',
+  //   },
+  // };
+  // const nativeNodeThree = {
+  //   id: 3,
+  //   pId: rootViewId,
+  //   index: 2,
+  //   name: 'Text',
+  //   props: {
+  //     text: 'Hello3',
+  //   },
+  // };
+  // // 初始化完成，插入节点
+  // const insertNodes: Array<NativeNode> = [
+  //   nativeNodeOne,
+  //   nativeNodeTwo,
+  //   nativeNodeThree,
+  // ];
   // 插入节点
   // renderToNative(insertNodes, NodeOperateType.CREATE);
 
